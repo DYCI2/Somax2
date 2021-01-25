DYCI2/Somax2

commit 6b9d4bb318e8c561014eec62c107289711216014

Debug of GMM and FreezeActivityPattern passed + critical bugfix in scheduler to handle negative row indices in DistanceMergeAction

python/somax/somax/runtime/peak_selector.py
@@ -82,6 +82,7 @@ class MaxPeakSelector(AbstractFallbackPeakSelector):
         if peaks.is_empty():
             return None
         max_peak_value: float = np.max(peaks.scores)
+        print("max peak value", max_peak_value)
         self.logger.debug(f"[decide_default] Max peak value is {max_peak_value}.")
         max_peaks_idx: List[int] = np.argwhere(np.abs(peaks.scores - max_peak_value) < 0.001)
         peak_idx: int = random.choice(max_peaks_idx)

python/somax/somax/scheduler/base_scheduler.py
@@ -102,7 +102,9 @@ class BaseScheduler(ABC):
         player: Player = trigger_event.player
         try:
             event_and_transform: Optional[Tuple[CorpusEvent, AbstractTransform]]
-            event_and_transform = player.new_event(trigger_event.target_time)
+            # TODO: This was changed from `trigger_event.target_time` to `self.tick` to handle unexpected delays in scheduling
+            #       (for example while loading a corpus or changing a classifier)
+            event_and_transform = player.new_event(self.tick)
         except InvalidCorpus as e:
             self.logger.error(str(e))
             self._requeue_trigger_event(trigger_event)

max/somax/patchers/somax.player.compact.maxpat
@@ -40,38 +40,6 @@
 		"assistshowspatchername" : 0,
 		"boxes" : [ 			{
 				"box" : 				{
-					"id" : "obj-67",
-					"maxclass" : "comment",
-					"numinlets" : 1,
-					"numoutlets" : 0,
-					"patching_rect" : [ 689.0, 343.0, 150.0, 20.0 ],
-					"presentation" : 1,
-					"presentation_rect" : [ 36.917322172757451, 76.749998927116394, 76.252622024363063, 20.0 ],
-					"text" : "(Disabled)",
-					"textcolor" : [ 1.0, 1.0, 1.0, 1.0 ]
-				}
-
-			}
-, 			{
-				"box" : 				{
-					"angle" : 270.0,
-					"bgcolor" : [ 0.368627450980392, 0.368627450980392, 0.368627450980392, 0.82 ],
-					"id" : "obj-70",
-					"ignoreclick" : 0,
-					"maxclass" : "panel",
-					"mode" : 0,
-					"numinlets" : 1,
-					"numoutlets" : 0,
-					"patching_rect" : [ 365.0, 65.454020529985428, 23.0, 24.673327137546494 ],
-					"presentation" : 1,
-					"presentation_rect" : [ 4.834644345515017, 52.454019457101822, 140.417977678847933, 78.295980542898178 ],
-					"proportion" : 0.5,
-					"rounded" : 0
-				}
-
-			}
-, 			{
-				"box" : 				{
 					"id" : "obj-68",
 					"maxclass" : "message",
 					"numinlets" : 2,
@@ -1124,7 +1092,7 @@
 					"patching_rect" : [ 430.5, 1084.735836863517761, 56.0, 24.0 ],
 					"presentation" : 1,
 					"presentation_rect" : [ 62.933834360329627, 275.025000810623169, 70.0, 24.0 ],
-					"text" : "2",
+					"text" : "9",
 					"textcolor" : [ 0.933333333333333, 0.933333333333333, 0.933333333333333, 1.0 ]
 				}
 
@@ -1472,7 +1440,7 @@
 					"patching_rect" : [ 692.433333333333394, 883.0, 49.0, 18.0 ],
 					"presentation" : 1,
 					"presentation_rect" : [ 10.248049099794571, 321.587493628263474, 40.0, 18.0 ],
-					"text" : "0",
+					"text" : "5",
 					"textcolor" : [ 0.481437705647327, 0.846666309931507, 0.361908344245232, 1.0 ],
 					"textjustification" : 1
 				}
@@ -1489,7 +1457,7 @@
 					"patching_rect" : [ 768.287878787878867, 883.0, 49.0, 18.0 ],
 					"presentation" : 1,
 					"presentation_rect" : [ 55.75262202436295, 321.587493628263474, 40.0, 18.0 ],
-					"text" : "2",
+					"text" : "0",
 					"textcolor" : [ 0.996078431372549, 0.423529411764706, 0.423529411764706, 1.0 ],
 					"textjustification" : 1
 				}
@@ -1523,7 +1491,7 @@
 					"patching_rect" : [ 616.333333333333371, 883.0, 49.0, 18.0 ],
 					"presentation" : 1,
 					"presentation_rect" : [ 163.333830729981287, 321.587493628263474, 40.0, 18.0 ],
-					"text" : "2",
+					"text" : "1",
 					"textcolor" : [ 0.933333333333333, 0.933333333333333, 0.933333333333333, 1.0 ],
 					"textjustification" : 1
 				}
@@ -3343,36 +3311,19 @@
 , 			{
 				"boxes" : [ "obj-20", "obj-535", "obj-509" ]
 			}
-, 			{
-				"boxes" : [ "obj-70", "obj-67" ]
-			}
  ],
 		"styles" : [ 			{
 				"name" : "dUG Yello 01-1",
 				"newobj" : 				{
-					"fontface" : [ 1 ],
-					"fontsize" : [ 10.0 ],
 					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
-					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ]
-				}
-,
-				"message" : 				{
+					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
 					"fontface" : [ 1 ],
-					"bgfillcolor" : 					{
-						"type" : "gradient",
-						"color" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
-						"color1" : [ 0.94902, 0.992157, 1.0, 1.0 ],
-						"color2" : [ 0.862745, 0.870588, 0.878431, 1.0 ],
-						"angle" : 270.0,
-						"proportion" : 0.39,
-						"autogradient" : 0
-					}
-,
-					"fontsize" : [ 10.0 ],
-					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ]
+					"fontsize" : [ 10.0 ]
 				}
 ,
 				"default" : 				{
+					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
+					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
 					"fontface" : [ 1 ],
 					"bgfillcolor" : 					{
 						"type" : "gradient",
@@ -3384,9 +3335,7 @@
 						"autogradient" : 0
 					}
 ,
-					"fontsize" : [ 10.0 ],
-					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
-					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ]
+					"fontsize" : [ 10.0 ]
 				}
 ,
 				"button" : 				{
@@ -3395,6 +3344,22 @@
 ,
 				"toggle" : 				{
 					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ]
+				}
+,
+				"message" : 				{
+					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"fontface" : [ 1 ],
+					"bgfillcolor" : 					{
+						"type" : "gradient",
+						"color" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
+						"color1" : [ 0.94902, 0.992157, 1.0, 1.0 ],
+						"color2" : [ 0.862745, 0.870588, 0.878431, 1.0 ],
+						"angle" : 270.0,
+						"proportion" : 0.39,
+						"autogradient" : 0
+					}
+,
+					"fontsize" : [ 10.0 ]
 				}
 ,
 				"parentstyle" : "",

max/somax/resources/player/somax.atom.maxpat
@@ -4,7 +4,7 @@
 		"appversion" : 		{
 			"major" : 8,
 			"minor" : 1,
-			"revision" : 5,
+			"revision" : 7,
 			"architecture" : "x64",
 			"modernui" : 1
 		}
@@ -39,6 +39,114 @@
 		"subpatcher_template" : "",
 		"assistshowspatchername" : 0,
 		"boxes" : [ 			{
+				"box" : 				{
+					"id" : "obj-54",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2188.0, 956.0, 126.0, 22.0 ],
+					"text" : "append override= true"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-53",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2188.0, 920.0, 308.0, 22.0 ],
+					"text" : "prepend create_atom player= #1 path= #2"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-51",
+					"maxclass" : "newobj",
+					"numinlets" : 6,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2037.0, 845.0, 71.5, 22.0 ],
+					"text" : "join 6"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-49",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2529.005555555555929, 781.0, 106.0, 22.0 ],
+					"text" : "prepend enabled="
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-44",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2390.681747437342892, 766.0, 144.0, 22.0 ],
+					"text" : "prepend activity_pattern="
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-43",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2253.071611298950302, 781.0, 145.0, 22.0 ],
+					"text" : "prepend memory_space="
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-41",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2149.222222222222626, 766.0, 110.0, 22.0 ],
+					"text" : "prepend classifier="
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-40",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 2027.209270291060648, 781.0, 143.0, 22.0 ],
+					"text" : "prepend self_influenced="
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-39",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 1930.0, 766.0, 98.0, 22.0 ],
+					"text" : "prepend weight="
+				}
+
+			}
+, 			{
 				"box" : 				{
 					"color" : [ 0.980525732040405, 0.526075959205627, 0.0, 1.0 ],
 					"id" : "obj-35",
@@ -82,7 +190,7 @@
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 1845.071428571428669, 436.77673273780556, 228.0, 22.0 ],
+					"patching_rect" : [ 1845.071428571428669, 436.77673273780556, 277.0, 22.0 ],
 					"text" : "prepend set_param #1 #2::_enabled"
 				}
 
@@ -146,18 +254,6 @@
 					"outlettype" : [ "", "" ],
 					"patching_rect" : [ 1138.0, 248.0, 29.5, 22.0 ],
 					"text" : "t l l"
-				}
-
-			}
-, 			{
-				"box" : 				{
-					"id" : "obj-3",
-					"maxclass" : "newobj",
-					"numinlets" : 2,
-					"numoutlets" : 1,
-					"outlettype" : [ "" ],
-					"patching_rect" : [ 1969.222222222222399, 1006.0, 37.0, 22.0 ],
-					"text" : "join 2"
 				}
 
 			}
@@ -442,7 +538,7 @@
 					"numinlets" : 6,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 1931.222222222222399, 845.0, 84.0, 22.0 ],
+					"patching_rect" : [ 1814.222222222222399, 857.0, 84.0, 22.0 ],
 					"text" : "pack f i s s s s"
 				}
 
@@ -556,7 +652,7 @@
 					"numinlets" : 2,
 					"numoutlets" : 2,
 					"outlettype" : [ "", "" ],
-					"patching_rect" : [ 2494.0, 360.433105802047749, 184.0, 35.0 ],
+					"patching_rect" : [ 2494.0, 360.433105802047749, 192.0, 35.0 ],
 					"text" : "combine #2::_classifier:: param @triggers 1"
 				}
 
@@ -638,24 +734,13 @@
 			}
 , 			{
 				"box" : 				{
-					"id" : "obj-70",
-					"maxclass" : "comment",
-					"numinlets" : 1,
-					"numoutlets" : 0,
-					"patching_rect" : [ 1977.222222222222399, 991.0, 150.0, 20.0 ],
-					"text" : "kwargs"
-				}
-
-			}
-, 			{
-				"box" : 				{
 					"id" : "obj-63",
 					"linecount" : 5,
 					"maxclass" : "message",
 					"numinlets" : 2,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 1931.222222222222399, 899.0, 202.000000000000227, 76.0 ],
+					"patching_rect" : [ 1814.222222222222399, 911.0, 206.000000000000227, 76.0 ],
 					"text" : "create_atom player= #1 path= #2 weight= $1 self_influenced= $2 classifier= $3 memory_space= $4 activity_pattern= $5 enabled= $6 override= true"
 				}
 
@@ -742,7 +827,7 @@
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 844.644444444444503, 577.456739086868993, 153.0, 35.0 ],
+					"patching_rect" : [ 844.644444444444503, 577.456739086868993, 171.0, 35.0 ],
 					"text" : "prepend set_param #1 #2::_self_influenced"
 				}
 
@@ -755,7 +840,7 @@
 					"numinlets" : 1,
 					"numoutlets" : 1,
 					"outlettype" : [ "" ],
-					"patching_rect" : [ 573.0, 573.456739086868993, 124.0, 35.0 ],
+					"patching_rect" : [ 573.0, 573.456739086868993, 150.0, 35.0 ],
 					"text" : "prepend set_param #1 #2::_weight"
 				}
 
@@ -912,16 +997,14 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-15", 3 ],
-					"midpoints" : [ 2262.571611298950302, 820.5, 1979.722222222222399, 820.5 ],
+					"destination" : [ "obj-43", 0 ],
 					"source" : [ "obj-11", 0 ]
 				}
 
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-15", 2 ],
-					"midpoints" : [ 2158.722222222222626, 808.5, 1966.722222222222399, 808.5 ],
+					"destination" : [ "obj-41", 0 ],
 					"source" : [ "obj-12", 0 ]
 				}
 
@@ -950,8 +1033,7 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-15", 4 ],
-					"midpoints" : [ 2400.181747437342892, 832.5, 1992.722222222222399, 832.5 ],
+					"destination" : [ "obj-44", 0 ],
 					"source" : [ "obj-14", 0 ]
 				}
 
@@ -1126,7 +1208,7 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-15", 5 ],
+					"destination" : [ "obj-49", 0 ],
 					"source" : [ "obj-35", 0 ]
 				}
 
@@ -1170,6 +1252,13 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-51", 0 ],
+					"source" : [ "obj-39", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-17", 0 ],
 					"source" : [ "obj-4", 0 ]
 				}
@@ -1184,8 +1273,40 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-51", 1 ],
+					"midpoints" : [ 2036.709270291060648, 795.5, 2057.0, 795.5 ],
+					"source" : [ "obj-40", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-51", 2 ],
+					"midpoints" : [ 2158.722222222222626, 808.5, 2067.5, 808.5 ],
+					"source" : [ "obj-41", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-36", 0 ],
 					"source" : [ "obj-42", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-51", 3 ],
+					"midpoints" : [ 2262.571611298950302, 820.5, 2078.0, 820.5 ],
+					"source" : [ "obj-43", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-51", 4 ],
+					"midpoints" : [ 2400.181747437342892, 832.5, 2088.5, 832.5 ],
+					"source" : [ "obj-44", 0 ]
 				}
 
 			}
@@ -1249,6 +1370,13 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-51", 5 ],
+					"source" : [ "obj-49", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-10", 0 ],
 					"source" : [ "obj-5", 2 ]
 				}
@@ -1291,6 +1419,13 @@
 			}
 , 			{
 				"patchline" : 				{
+					"destination" : [ "obj-53", 0 ],
+					"source" : [ "obj-51", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
 					"destination" : [ "obj-16", 0 ],
 					"source" : [ "obj-52", 0 ]
 				}
@@ -1300,6 +1435,20 @@
 				"patchline" : 				{
 					"destination" : [ "obj-24", 0 ],
 					"source" : [ "obj-52", 1 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-54", 0 ],
+					"source" : [ "obj-53", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-29", 0 ],
+					"source" : [ "obj-54", 0 ]
 				}
 
 			}
@@ -1351,16 +1500,7 @@
 , 			{
 				"patchline" : 				{
 					"destination" : [ "obj-29", 0 ],
-					"midpoints" : [ 1940.722222222222399, 1022.0, 582.5, 1022.0 ],
-					"order" : 1,
-					"source" : [ "obj-63", 0 ]
-				}
-
-			}
-, 			{
-				"patchline" : 				{
-					"destination" : [ "obj-3", 0 ],
-					"order" : 0,
+					"midpoints" : [ 1823.722222222222399, 1022.0, 582.5, 1022.0 ],
 					"source" : [ "obj-63", 0 ]
 				}
 
@@ -1411,7 +1551,7 @@
 , 			{
 				"patchline" : 				{
 					"destination" : [ "obj-75", 1 ],
-					"midpoints" : [ 2503.5, 351.966552901023817, 2668.5, 351.966552901023817 ],
+					"midpoints" : [ 2503.5, 351.966552901023817, 2676.5, 351.966552901023817 ],
 					"source" : [ "obj-73", 0 ]
 				}
 
@@ -1439,8 +1579,7 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-15", 1 ],
-					"midpoints" : [ 2036.709270291060648, 795.5, 1953.722222222222399, 795.5 ],
+					"destination" : [ "obj-40", 0 ],
 					"source" : [ "obj-8", 0 ]
 				}
 
@@ -1455,7 +1594,7 @@
 			}
 , 			{
 				"patchline" : 				{
-					"destination" : [ "obj-15", 0 ],
+					"destination" : [ "obj-39", 0 ],
 					"source" : [ "obj-9", 0 ]
 				}
 

max/somax/somax2.wireless.maxpat
@@ -10,7 +10,7 @@
 		}
 ,
 		"classnamespace" : "box",
-		"rect" : [ 34.0, -221.0, 1372.0, 731.0 ],
+		"rect" : [ 1037.0, 78.0, 1365.0, 977.0 ],
 		"bglocked" : 0,
 		"openinpresentation" : 1,
 		"default_fontsize" : 12.0,
@@ -515,6 +515,13 @@
 				"implicit" : 1
 			}
 , 			{
+				"name" : "somax.player.routerblock.maxpat",
+				"bootpath" : "~/Documents/Max 8/Packages/Somax2/max/somax/resources/player",
+				"patcherrelativepath" : "./resources/player",
+				"type" : "JSON",
+				"implicit" : 1
+			}
+, 			{
 				"name" : "somax.player.compact.maxpat",
 				"bootpath" : "~/Documents/Max 8/Packages/Somax2/max/somax/patchers",
 				"patcherrelativepath" : "./patchers",
@@ -617,6 +624,13 @@
 				"bootpath" : "~/Documents/Max 8/Packages/Somax2/max/somax/patchers",
 				"patcherrelativepath" : "./patchers",
 				"type" : "JSON",
+				"implicit" : 1
+			}
+, 			{
+				"name" : "Sax_gentle cat_Slow.wav",
+				"bootpath" : "~/Music/Monophonic",
+				"patcherrelativepath" : "../../../../../../Music/Monophonic",
+				"type" : "WAVE",
 				"implicit" : 1
 			}
 , 			{
@@ -728,11 +742,16 @@
 		"autosave" : 0,
 		"styles" : [ 			{
 				"name" : "dUG Yello 01-1",
-				"default" : 				{
-					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
-					"fontface" : [ 1 ],
-					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
+				"newobj" : 				{
 					"fontsize" : [ 10.0 ],
+					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
+					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"fontface" : [ 1 ]
+				}
+,
+				"message" : 				{
+					"fontsize" : [ 10.0 ],
+					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
 					"bgfillcolor" : 					{
 						"type" : "gradient",
 						"color" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
@@ -742,7 +761,25 @@
 						"proportion" : 0.39,
 						"autogradient" : 0
 					}
-
+,
+					"fontface" : [ 1 ]
+				}
+,
+				"default" : 				{
+					"fontsize" : [ 10.0 ],
+					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
+					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"bgfillcolor" : 					{
+						"type" : "gradient",
+						"color" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
+						"color1" : [ 0.94902, 0.992157, 1.0, 1.0 ],
+						"color2" : [ 0.862745, 0.870588, 0.878431, 1.0 ],
+						"angle" : 270.0,
+						"proportion" : 0.39,
+						"autogradient" : 0
+					}
+,
+					"fontface" : [ 1 ]
 				}
 ,
 				"button" : 				{
@@ -751,29 +788,6 @@
 ,
 				"toggle" : 				{
 					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ]
-				}
-,
-				"newobj" : 				{
-					"bgcolor" : [ 1.0, 0.941176, 0.803922, 1.0 ],
-					"fontface" : [ 1 ],
-					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
-					"fontsize" : [ 10.0 ]
-				}
-,
-				"message" : 				{
-					"fontface" : [ 1 ],
-					"textcolor_inverse" : [ 0.0, 0.0, 0.0, 1.0 ],
-					"fontsize" : [ 10.0 ],
-					"bgfillcolor" : 					{
-						"type" : "gradient",
-						"color" : [ 0.290196, 0.309804, 0.301961, 1.0 ],
-						"color1" : [ 0.94902, 0.992157, 1.0, 1.0 ],
-						"color2" : [ 0.862745, 0.870588, 0.878431, 1.0 ],
-						"angle" : 270.0,
-						"proportion" : 0.39,
-						"autogradient" : 0
-					}
-
 				}
 ,
 				"parentstyle" : "",

python/somax/somax/classification/__init__.py
@@ -1,3 +1,3 @@
-from .chroma_classifiers import SomChromaClassifier
+from .chroma_classifiers import SomChromaClassifier, AbsoluteGmmClassifier, RelativeGmmClassifier
 from .pitch_classifiers import TopNoteClassifier, PitchClassClassifier, VirtualFundamentalClassifier, \
     BassNoteClassifier, BassNoteMod12Classifier

python/somax/somax/classification/chroma_classifiers.py
@@ -11,7 +11,7 @@ from somax.features import BackgroundChroma
 from somax.features.feature import RuntimeFeature, CorpusFeature
 from somax.runtime.corpus import Corpus
 from somax.runtime.corpus_event import CorpusEvent
-from somax.runtime.exceptions import InvalidLabelInput, TransformError
+from somax.runtime.exceptions import InvalidLabelInput, TransformError, InvalidCorpus
 from somax.runtime.influence import AbstractInfluence, CorpusInfluence, FeatureInfluence
 from somax.runtime.label import AbstractLabel, IntLabel
 from somax.runtime.transform_handler import TransformHandler
@@ -157,9 +157,20 @@ class AbsoluteGmmClassifier(GmmClassifier):
 class RelativeGmmClassifier(GmmClassifier):
 
     def cluster(self, corpus: Corpus) -> None:
+        """ :raises InvalidCorpus if number of events in corpus is lower than `self.num_components`."""
         chromas: List[np.ndarray] = [event.get_feature(BackgroundChroma).value() for event in corpus.events]
         gmm_data: np.ndarray = np.row_stack(chromas)
         max_per_col: np.ndarray = np.max(chromas, axis=1)
         max_per_col[max_per_col == 0] = 1  # don't normalize empty vectors - avoid div0 error
         chromas /= max_per_col[:, np.newaxis]
-        self.gmm = GaussianMixture(n_components=self.num_components, max_iter=self.max_iter).fit(gmm_data)
+        try:
+            self.gmm = GaussianMixture(n_components=self.num_components, max_iter=self.max_iter).fit(gmm_data)
+        except ValueError as e:
+            if self.num_components > corpus.length():
+                raise InvalidCorpus(f"{self.__class__.__name__} could not classify corpus '{str(corpus)}' since corpus "
+                                    f"length ({corpus.length()}) is lower than number of requested clusters "
+                                    f"({self.num_components}). "
+                                    f"Reduce the number of clusters or select another classifier")
+            else:
+                raise InvalidCorpus(f"Unknown error encountered in {self.__class__.__name__}. Error: {repr(e)}.")
+

commit da255973fad85c9d4976456aa65391058e21ebbb

Fixed critical scheduling bug introduced by 6b9d4bb

python/somax/somax/runtime/peak_selector.py
@@ -82,7 +82,6 @@ class MaxPeakSelector(AbstractFallbackPeakSelector):
         if peaks.is_empty():
             return None
         max_peak_value: float = np.max(peaks.scores)
-        print("max peak value", max_peak_value)
         self.logger.debug(f"[decide_default] Max peak value is {max_peak_value}.")
         max_peaks_idx: List[int] = np.argwhere(np.abs(peaks.scores - max_peak_value) < 0.001)
         peak_idx: int = random.choice(max_peaks_idx)

python/somax/somax/scheduler/base_scheduler.py
@@ -102,9 +102,10 @@ class BaseScheduler(ABC):
         player: Player = trigger_event.player
         try:
             event_and_transform: Optional[Tuple[CorpusEvent, AbstractTransform]]
-            # TODO: This was changed from `trigger_event.target_time` to `self.tick` to handle unexpected delays in scheduling
-            #       (for example while loading a corpus or changing a classifier)
-            event_and_transform = player.new_event(self.tick)
+            # By default, target time should always be the time given by the trigger_event, but may occasionally use
+            #   self.tick to handle unexpected delays in scheduling (for example while loading a corpus)
+            target_time: float = max(trigger_event.target_time, self.tick)
+            event_and_transform = player.new_event(target_time)
         except InvalidCorpus as e:
             self.logger.error(str(e))
             self._requeue_trigger_event(trigger_event)