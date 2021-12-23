96d7ee714bc301a0220a2eb1b8e916deec247ad3
jobor019 <joakimborg@hotmail.com>
2021-12-23 09:42:19 +0100

    Support for python 3.7 added

--- python/somax/setup.py
+++ python/somax/setup.py
@@ -10,13 +10,15 @@ with open('README.md') as readme_file:
 setup(
     author="Joakim Borg",
     author_email='joakim.borg@ircam.fr',
-    python_requires='>=3.9',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     description="Somax Library",
@@ -30,6 +32,6 @@ setup(
     name='somax',
     packages=find_packages(include=['somax', 'somax.*']),
     url='https://github.com/DYCI2/Somax2',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
